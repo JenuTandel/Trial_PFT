697723b76e292ceaabb0d119e64bce729e1669a6
Jinal Tandel <jinal.tandel@1rivet.com>
2024-05-10 17:56:40 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Auth0Provider } from '@auth0/auth0-react';
+import { AppState, Auth0Provider } from '@auth0/auth0-react';
 import { AppShell, MantineProvider } from '@mantine/core';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,8 +7,16 @@ import { resolver, theme } from './core/utility/constants/core.constant';
 import { store } from './store/store';
 
 function App() {
+  // const navigate = useNavigate();
+  const onRedirectCallback = (appState?: AppState) => {
+    console.log(appState);
+
+    // navigate(
+    //   appState?.returnTo || window.location.pathname + window.location.search
+    // );
+  };
   return (
-    <Auth0Provider clientId='7r31d0stc7uRmxV4ImSuKhiSdaNWsxFM' domain='dev-vf8j6fx765bq08e6.us.auth0.com' authorizationParams={{ redirect_uri: 'http://localhost:5173/callback' }}>
+    <Auth0Provider clientId='7r31d0stc7uRmxV4ImSuKhiSdaNWsxFM' domain='dev-vf8j6fx765bq08e6.us.auth0.com' onRedirectCallback={onRedirectCallback} authorizationParams={{ redirect_uri: 'http://localhost:5173/callback' }}>
       <Provider store={store}>
         <BrowserRouter>
           <MantineProvider theme={theme} cssVariablesResolver={resolver}>
--- src/core/components/auth/AuthCallback.tsx
+++ src/core/components/auth/AuthCallback.tsx
@@ -2,33 +2,29 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { setRole, setToken } from '../../../features/auth/auth';
-import { useAppDispatch, useAppSelector } from '../../../store/store';
+import { useAppDispatch } from '../../../store/store';
 
 function AuthCallback() {
   const dispatch = useAppDispatch();
-  const user = useAppSelector((state) => state.auth.access_token)
   const navigate = useNavigate();
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
-  console.log("Auth Callback");
+  const { isAuthenticated, getAccessTokenSilently, isLoading, error } = useAuth0();
+
   useEffect(() => {
-    console.log("useEffect");
-    if (!user) {
+    if (!isLoading && !error && isAuthenticated) {
+      const handleAfterLogin = async () => {
+        try {
+          const claims = await getAccessTokenSilently();
+          dispatch(setToken(claims));
+          dispatch(setRole('Admin'));
+          navigate('/');
+        } catch (error) {
+          console.error('Error getting access token:', error);
+        }
+      };
+
       handleAfterLogin();
     }
-    async function handleAfterLogin() {
-      debugger;
-      console.log("After login");
-      console.log("Authenticated", isAuthenticated);
-      if (isAuthenticated) {
-        debugger;
-        const claims = await getAccessTokenSilently();
-        console.log('User:', claims);
-        dispatch(setToken(claims));
-        dispatch(setRole('Admin'))
-        navigate('/');
-      }
-    }
-  }, [isAuthenticated, getAccessTokenSilently]);
+  }, [isLoading, isAuthenticated, getAccessTokenSilently, dispatch, navigate, error]);
 
   return <div>Loading...</div>;
 }
